ebb56e42e65154aabb83fc6a81d881bd4ef4189f
huyld1504 <ledinhhuy0105@gmail.com>
2026-03-01 18:49:53 +0700

    fix env uri

--- constants/constant.js
+++ constants/constant.js
@@ -1,5 +1,10 @@
 const API_CONSTANT = {
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/SimpleQuiz',
+  get MONGO_URI() {
+    return process.env.MONGO_URI || 'mongodb://localhost:27017/assignment4';
+  },
+  get JWT_SECRET() {
+    return process.env.JWT_SECRET || 'your_jwt_secret';
+  }
 }
 
 export default API_CONSTANT;
